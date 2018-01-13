67c7fa87b8b59f23ecf546883c358b4f1152c48c
Ethan Jobmann <ethanlouis_fj@yahoo.com>
2018-01-13 17:38:27 -0500

    set up angular & angular controller

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,7 @@
 console.log("app.js is linked");
+
+const app = angular.module('community-app', []);
+
+app.controller('MainController', ['$http', function($http) {
+  this.test = 'angular is running';
+}]);
