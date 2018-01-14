314acecf4ea43fdc7dca5d674f92a0f2b08a4b81
Anthony <ajtalvarez@gmail.com>
2018-01-14 00:47:07 -0500

    Inherited server, set up Angular between index and app.

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,7 @@
 console.log("app.js is linked");
+
+const app = angular.module("comm_app", []);
+
+app.controller("mainController", ["$http", function($http) {
+  this.test = "This is a test of the Angular broadcast system."
+}]);
