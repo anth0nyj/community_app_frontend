022023d89996531756d23ddf676535b8831908a0
Anthony <ajtalvarez@gmail.com>
2018-01-14 14:34:05 -0500

    Token saved to localStorage upon form submission.

--- public/js/app.js
+++ public/js/app.js
@@ -3,5 +3,21 @@ console.log("app.js is linked");
 const app = angular.module("comm_app", []);
 
 app.controller("mainController", ["$http", function($http) {
-  this.test = "This is a test of the Angular broadcast system."
+  this.url = 'http://localhost:3000';
+  this.user = {};
+
+  this.login = function(userPass) {
+    console.log(userPass);
+
+    $http({
+      method: 'POST',
+      url: this.url + '/users/login',
+      data: { user: { username: userPass.username, password: userPass.password }},
+    }).then(response => {
+      console.log(response);
+      this.user = response.data.user;
+      localStorage.setItem('token', JSON.stringify(response.data.token));
+    });
+  }
+
 }]);
